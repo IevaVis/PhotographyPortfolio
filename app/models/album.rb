4e4a9e3bd8d714953acf4ce08a411ff514c4bc6b
IevaVis <ieva.visniauskiene@gmail.com>
2018-07-24 11:44:17 +0300

    added error messages

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,12 @@ class Album < ApplicationRecord
 	belongs_to :admin
 	has_many_attached :images
 	validates :title, :body, presence: true
+	validate :image_type
+
+	private
+	def image_type
+		if images.attached? == false
+			errors.add(:images, "are missing!")
+		end
+	end
 end
